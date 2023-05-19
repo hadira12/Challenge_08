aacb419ad81772c91f1e928f745d53f8b0dfbea7
Hady RA <hadyagusti12@gmail.com>
2023-05-19 15:26:05 +0700

    first commit

---
+++ app/migrator.spec.js
@@ -0,0 +1,14 @@
+const migrator = require("./migrator");
+
+// jest.setTimeout(20000);
+jest.useFakeTimers("legacy");
+
+describe("test migrator", () => {
+  // beforeEach(async () => {
+  //     return migrator()
+  // })
+
+  it("test", (done) => {
+    migrator().then(done).catch(done);
+  });
+});
